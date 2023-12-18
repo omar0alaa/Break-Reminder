05ccf728cda37a376871b97fe1902d1908cfdd1a
Omar Khater <omar0alaa@gmail.com>
2023-12-18 18:38:02 +0800

    Fixed Path to images issue

--- popup.js
+++ popup.js
@@ -1,16 +1,16 @@
-let intervalId;
-
 document.getElementById('startBtn').addEventListener('click', startReminders);
 document.getElementById('stopBtn').addEventListener('click', stopReminders);
 
 // Load user settings on popup open
 chrome.storage.sync.get(['breakInterval'], function(result) {
+  console.log('Retrieved breakInterval:', result.breakInterval);
   const savedInterval = result.breakInterval || 30; // Default to 30 minutes
   document.getElementById('intervalInput').value = savedInterval;
 });
 
 function startReminders() {
   const breakInterval = parseInt(document.getElementById('intervalInput').value, 10) * 60 * 1000;
+  console.log('Starting reminders with breakInterval:', breakInterval);
 
   // Save user setting
   chrome.storage.sync.set({ 'breakInterval': breakInterval });
@@ -22,12 +22,14 @@ function startReminders() {
 
 function stopReminders() {
   clearInterval(intervalId);
+  console.log('Reminders stopped.');
 }
 
 function showNotification(title, message) {
+  console.log('Showing notification:', title, message);
   chrome.notifications.create({
     type: 'basic',
-    iconUrl: 'images/icon48.png',
+    iconUrl: 'images/Icon-44.png',
     title: title,
     message: message
   });
