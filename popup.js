a42d542b5b2c3a5be3acad45b6f5f283f38d18df
Omar Khater <omar0alaa@gmail.com>
2023-12-18 17:46:33 +0800

    added User setting and save in chrome storage

--- popup.js
+++ popup.js
@@ -1,12 +1,23 @@
 let intervalId;
 
-document.getElementById("startBtn").addEventListener("click", startReminders);
-document.getElementById("stopBtn").addEventListener("click", stopReminders);
+document.getElementById('startBtn').addEventListener('click', startReminders);
+document.getElementById('stopBtn').addEventListener('click', stopReminders);
+
+// Load user settings on popup open
+chrome.storage.sync.get(['breakInterval'], function(result) {
+  const savedInterval = result.breakInterval || 30; // Default to 30 minutes
+  document.getElementById('intervalInput').value = savedInterval;
+});
 
 function startReminders() {
+  const breakInterval = parseInt(document.getElementById('intervalInput').value, 10) * 60 * 1000;
+
+  // Save user setting
+  chrome.storage.sync.set({ 'breakInterval': breakInterval });
+
   intervalId = setInterval(() => {
-    showNotification("Take a break!", "It's time to relax for a moment.");
-  }, 30 * 60 * 1000); // Remind every 30 minutes
+    showNotification('Take a break!', 'It\'s time to relax for a moment.');
+  }, breakInterval);
 }
 
 function stopReminders() {
@@ -15,9 +26,9 @@ function stopReminders() {
 
 function showNotification(title, message) {
   chrome.notifications.create({
-    type: "basic",
-    iconUrl: "images/icon-44.png",
+    type: 'basic',
+    iconUrl: 'images/icon48.png',
     title: title,
-    message: message,
+    message: message
   });
 }
