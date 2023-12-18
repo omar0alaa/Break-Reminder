36d69904cc0323d0089d72fba0465ac0d28b780f
Omar Khater <omar0alaa@gmail.com>
2023-12-18 20:25:58 +0800

    Added timer for next notification.

--- background.js
+++ background.js
@@ -3,17 +3,23 @@ chrome.runtime.onInstalled.addListener(function () {
     chrome.tabs.create({ 'url': chrome.extension.getURL('popup.html'), 'selected': true });
   });
 
-  // Move the interval setup to the background script
   let intervalId;
+  let nextNotificationTime;
 
   function startReminders(breakInterval) {
     intervalId = setInterval(() => {
       showNotification('Take a break!', 'It\'s time to relax for a moment.');
+      nextNotificationTime = Date.now() + breakInterval;
+      updateNextNotificationTime(); // Notify popup about the next notification time
     }, breakInterval);
+    nextNotificationTime = Date.now() + breakInterval;
+    updateNextNotificationTime(); // Notify popup about the initial next notification time
   }
 
   function stopReminders() {
     clearInterval(intervalId);
+    nextNotificationTime = undefined;
+    updateNextNotificationTime(); // Notify popup that reminders are stopped
   }
 
   function showNotification(title, message) {
@@ -25,12 +31,20 @@ chrome.runtime.onInstalled.addListener(function () {
     });
   }
 
-  // Message listener to start/stop reminders
-  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action === 'startReminders') {
-      startReminders(request.breakInterval);
-    } else if (request.action === 'stopReminders') {
-      stopReminders();
-    }
+  function updateNextNotificationTime() {
+    chrome.runtime.sendMessage({ action: 'updateNextNotificationTime', nextNotificationTime });
+  }
+
+  // Message listener to start/stop reminders and get next notification time
+  chrome.runtime.onConnect.addListener(function (port) {
+    port.onMessage.addListener(function (msg) {
+      if (msg.action === 'startReminders') {
+        startReminders(msg.breakInterval);
+      } else if (msg.action === 'stopReminders') {
+        stopReminders();
+      } else if (msg.action === 'getNextNotificationTime') {
+        port.postMessage({ action: 'updateNextNotificationTime', nextNotificationTime });
+      }
+    });
   });
 });
--- popup.js
+++ popup.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
+  const port = chrome.runtime.connect({ name: 'popup' });
+
   // Load user settings on popup open
   chrome.storage.sync.get(['breakInterval'], function (result) {
     const savedInterval = result.breakInterval || 30 * 60 * 1000; // Default to 30 minutes
-    // Convert milliseconds to minutes for display
     const savedIntervalMinutes = savedInterval / (60 * 1000);
     document.getElementById('intervalInput').value = savedIntervalMinutes;
   });
@@ -18,11 +19,51 @@ document.addEventListener('DOMContentLoaded', function () {
     chrome.storage.sync.set({ 'breakInterval': breakInterval });
 
     // Inform the background script to start reminders
-    chrome.runtime.sendMessage({ action: 'startReminders', breakInterval });
+    port.postMessage({ action: 'startReminders', breakInterval });
+
+    // Request the next notification time and start the timer
+    requestNextNotificationTime();
   }
 
   function stopReminders() {
     // Inform the background script to stop reminders
-    chrome.runtime.sendMessage({ action: 'stopReminders' });
+    port.postMessage({ action: 'stopReminders' });
+
+    // Reset the timer display
+    updateNextNotificationTimer(undefined);
+  }
+
+  function requestNextNotificationTime() {
+    // Request the next notification time from the background script
+    port.postMessage({ action: 'getNextNotificationTime' });
+  }
+
+  // Message listener to update the next notification time and start the timer
+  port.onMessage.addListener(function (msg) {
+    if (msg.action === 'updateNextNotificationTime') {
+      updateNextNotificationTimer(msg.nextNotificationTime);
+      // Continue requesting the next notification time to keep the timer updated
+      requestNextNotificationTime();
+    }
+  });
+
+  function updateNextNotificationTimer(time) {
+    const nextNotificationElement = document.getElementById('nextNotification');
+
+    function formatTime(time) {
+      const hours = Math.floor(time / (60 * 60 * 1000));
+      const minutes = Math.floor((time % (60 * 60 * 1000)) / (60 * 1000));
+      const seconds = Math.floor((time % (60 * 1000)) / 1000);
+
+      return `${hours}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    }
+
+    if (time) {
+      const timeUntilNextNotification = time - Date.now();
+      const formattedTime = formatTime(Math.max(timeUntilNextNotification, 0));
+      nextNotificationElement.textContent = `Next notification in: ${formattedTime}`;
+    } else {
+      nextNotificationElement.textContent = '';
+    }
   }
 });
