2a01d26c5f1c79c8fae4b9573d87eabff64d17d4
Omar Khater <omar0alaa@gmail.com>
2023-12-18 20:12:29 +0800

    Roll Back to Manifest 2 and fixed issues with background

--- background.js
+++ background.js
@@ -1,6 +1,36 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.browserAction.onClicked.addListener(function(tab) {
-      chrome.tabs.create({ 'url': chrome.extension.getURL('popup.html'), 'selected': true });
-    });
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.tabs.create({ 'url': chrome.extension.getURL('popup.html'), 'selected': true });
   });
-  
+
+  // Move the interval setup to the background script
+  let intervalId;
+
+  function startReminders(breakInterval) {
+    intervalId = setInterval(() => {
+      showNotification('Take a break!', 'It\'s time to relax for a moment.');
+    }, breakInterval);
+  }
+
+  function stopReminders() {
+    clearInterval(intervalId);
+  }
+
+  function showNotification(title, message) {
+    chrome.notifications.create({
+      type: 'basic',
+      iconUrl: 'images/Icon-44.png',
+      title: title,
+      message: message
+    });
+  }
+
+  // Message listener to start/stop reminders
+  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action === 'startReminders') {
+      startReminders(request.breakInterval);
+    } else if (request.action === 'stopReminders') {
+      stopReminders();
+    }
+  });
+});
--- popup.js
+++ popup.js
@@ -1,33 +1,28 @@
-document.getElementById('startBtn').addEventListener('click', startReminders);
-document.getElementById('stopBtn').addEventListener('click', stopReminders);
-
-// Load user settings on popup open
-chrome.storage.sync.get(['breakInterval'], function(result) {
-  const savedInterval = result.breakInterval || 30; // Default to 30 minutes
-  document.getElementById('intervalInput').value = savedInterval;
-});
-
-function startReminders() {
-  const userInput = parseInt(document.getElementById('intervalInput').value, 10);
-  const breakInterval = Math.max(userInput, 1) * 60 * 1000; // Enforce a minimum of 1 minute
-
-  // Save user setting
-  chrome.storage.sync.set({ 'breakInterval': breakInterval });
-
-  intervalId = setInterval(() => {
-    showNotification('Take a break!', 'It\'s time to relax for a moment.');
-  }, breakInterval);
-}
-
-function stopReminders() {
-  clearInterval(intervalId);
-}
-
-function showNotification(title, message) {
-  chrome.notifications.create({
-    type: 'basic',
-    iconUrl: 'images/icon48.png',
-    title: title,
-    message: message
+document.addEventListener('DOMContentLoaded', function () {
+  // Load user settings on popup open
+  chrome.storage.sync.get(['breakInterval'], function (result) {
+    const savedInterval = result.breakInterval || 30 * 60 * 1000; // Default to 30 minutes
+    // Convert milliseconds to minutes for display
+    const savedIntervalMinutes = savedInterval / (60 * 1000);
+    document.getElementById('intervalInput').value = savedIntervalMinutes;
   });
-}
+
+  document.getElementById('startBtn').addEventListener('click', startReminders);
+  document.getElementById('stopBtn').addEventListener('click', stopReminders);
+
+  function startReminders() {
+    const userInput = parseInt(document.getElementById('intervalInput').value, 10);
+    const breakInterval = Math.max(userInput, 1) ;//* 60 * 1000; // Enforce a minimum of 1 minute
+
+    // Save user setting
+    chrome.storage.sync.set({ 'breakInterval': breakInterval });
+
+    // Inform the background script to start reminders
+    chrome.runtime.sendMessage({ action: 'startReminders', breakInterval });
+  }
+
+  function stopReminders() {
+    // Inform the background script to stop reminders
+    chrome.runtime.sendMessage({ action: 'stopReminders' });
+  }
+});
